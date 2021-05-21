959c0d508bff85c6144253c6658a536362a99578
Luiz Fernando <lfluiz.lf@gmail.com>
2021-05-20 22:47:03 -0300

    change the default test log

---
+++ libft/.tests/test_tolower.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "../ft_tolower.c"
+
+int main (char **argv)
+{
+	char teste;
+	printf("*********************************\n");
+	printf("@@@@@@@@@@@ FT_TOLOWER @@@@@@@@@@\n");
+	teste = ft_tolower('A');
+	printf("<ft_tolower.c> var=A, res=%c", teste);
+	printf("\n");
+	teste = tolower('A');
+	printf("<tolower.c> var=A, res=%c", teste);
+	printf("\n");
+	printf("---------------------------------\n");
+	teste = ft_tolower('B');
+	printf("<ft_tolower.c> var=B, res=%c", teste);
+	printf("\n");
+	teste = tolower('B');
+	printf("<tolower.c> var=B, res=%c", teste);
+	printf("\n");
+	printf("---------------------------------\n");
+	teste = ft_tolower('c');
+	printf("<ft_tolower.c> var=c, res=%c", teste);
+	printf("\n");
+	teste = tolower('c');
+	printf("<tolower.c> var=c, res=%c", teste);
+	printf("\n");
+	printf("*********************************\n");
+}
