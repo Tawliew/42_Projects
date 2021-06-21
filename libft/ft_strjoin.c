cbe8f0efc14114ba89a584fa68c581d6b58bd2c3
Luiz Fernando <lfluiz.lf@gmail.com>
2021-06-21 15:23:12 -0300

    refactoring the ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: luizfern <lfluiz.lf@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 14:43:01 by luizfern          #+#    #+#             */
-/*   Updated: 2021/06/21 15:12:27 by luizfern         ###   ########.fr       */
+/*   Updated: 2021/06/21 15:21:15 by luizfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*strconc;
 	int		a;
 	int		b;
-	int		c;
 
 	a = 0;
 	b = 0;
-	c = 0;
 	strconc = (char *)malloc(sizeof(s1) * sizeof(s2) + 1);
-	if (strconc == NULL)
+	if ((strconc == NULL) || (s1 == NULL || s2 == NULL))
 		return (NULL);
 	while ((unsigned char)s1[a])
 	{
-		strconc[a] = (unsigned char)s1[b];
+		strconc[a] = (unsigned char)s1[a];
+		a++;
+	}
+	while ((unsigned char)s2[b])
+	{
+		strconc[a] = (unsigned char)s2[b];
 		a++;
 		b++;
-	}
-	while ((unsigned char)s2[c])
-	{
-		strconc[a] = (unsigned char)s2[c];
-		a++;
-		c++;
 	}
 	strconc[a] = '\0';
 	return (strconc);
